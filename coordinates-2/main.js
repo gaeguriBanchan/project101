bc849fbf9a1689a7b25ff864b92613accfff61e4
gaeguriBanchan <rlawowls93@gmail.com>
2023-02-12 15:52:05 +0900

    4.8 실습6: 좌표 실습 성능 개선

---
+++ coordinates-2/main.js
@@ -0,0 +1,23 @@
+const xBar = document.querySelector('.xBar');
+const yBar = document.querySelector('.yBar');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    xBar.style.transform = `translateX(${x}px)`;
+    yBar.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+  });
+});
